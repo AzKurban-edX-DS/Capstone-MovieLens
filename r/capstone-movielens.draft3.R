1e5f1cc8a9030aa5e58d39340c11efd5b7dc9ce6
AzKurban2 <azkurban@gmail.com>
2025-03-26 11:54:00 +0300

    Minor fix for the Regularized User+Movie+Genre Effect Model.

--- r/capstone-movielens.draft3.R
+++ r/capstone-movielens.draft3.R
@@ -1405,8 +1405,8 @@ put_log1("Regularized User+Movie Effect Model has been re-trained for the best `
 put_log1("Is this a best RMSE? %1",
          best_user_movie_genre_reg_RMSE == user_movie_genre_effect_best_lambda_RMSE)
 
-#### Add a row to the RMSE Result Table for the Regularized User+Movie Effect Model --------
-RMSEs <- rmses_add_row("Regularized User+Movie Effect Model", 
+#### Add a row to the RMSE Result Table for the Regularized User+Movie+Genre Effect Model --------
+RMSEs <- rmses_add_row("Regularized User+Movie+Genre Effect Model", 
                        best_user_movie_genre_reg_RMSE)
 rmse_kable()
 
