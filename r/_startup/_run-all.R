c472edfedbf0ed0e4407eb04e9d988e1073f5030
AzKurban2 <azkurban@gmail.com>
2025-09-19 01:52:32 +0300

    Refactoring (continue): in progress...

---
+++ r/_startup/_run-all.R
@@ -0,0 +1,31 @@
+# Run All Scripts
+## Source File Paths -----------------------------------------------------------
+startup.path <- "r/_startup"
+init.script_path = file.path(startup.path, "1.init.R")
+main.script_path = file.path(startup.path, "2.run-main-script.R")
+rmd_render.script_path = file.path(startup.path, "3.rmd.render.R")
+
+## Run Scripts -----------------------------------------------------------------
+
+source(init.script_path, 
+       catch.aborts = TRUE,
+       echo = TRUE,
+       spaced = TRUE,
+       verbose = TRUE,
+       keep.source = TRUE)
+
+# source(path, 
+#        catch.aborts = TRUE,
+#        echo = TRUE,
+#        spaced = TRUE,
+#        verbose = TRUE,
+#        keep.source = TRUE)
+# 
+# source(path, 
+#        catch.aborts = TRUE,
+#        echo = TRUE,
+#        spaced = TRUE,
+#        verbose = TRUE,
+#        keep.source = TRUE)
+# 
+
