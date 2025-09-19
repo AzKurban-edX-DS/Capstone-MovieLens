5a029074d01e6944a7af56498804201ae12298b7
AzKurban2 <azkurban@gmail.com>
2025-09-19 04:46:22 +0300

    Fix for main script.

--- r/_startup/_run-all.R
+++ r/_startup/_run-all.R
@@ -1,9 +1,9 @@
 # Run All Scripts
 ## Source File Paths -----------------------------------------------------------
 startup.path <- "r/_startup"
-init.script_path = file.path(startup.path, "1.init.R")
-main.script_path = file.path(startup.path, "2.run-main-script.R")
-rmd_render.script_path = file.path(startup.path, "3.rmd.render.R")
+init.script_path <- file.path(startup.path, "1.init.R")
+run_main.script_path <- file.path(startup.path, "2.run-main-script.R")
+rmd_render.script_path < file.path(startup.path, "3.rmd.render.R")
 
 ## Run Scripts -----------------------------------------------------------------
 
@@ -14,13 +14,13 @@ source(init.script_path,
        verbose = TRUE,
        keep.source = TRUE)
 
-# source(path, 
-#        catch.aborts = TRUE,
-#        echo = TRUE,
-#        spaced = TRUE,
-#        verbose = TRUE,
-#        keep.source = TRUE)
-# 
+source(run_main.script_path,
+       catch.aborts = TRUE,
+       echo = TRUE,
+       spaced = TRUE,
+       verbose = TRUE,
+       keep.source = TRUE)
+
 # source(path, 
 #        catch.aborts = TRUE,
 #        echo = TRUE,
