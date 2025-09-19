244fa290e99b31bb66ccaaf255e4d50ea3322485
AzKurban2 <azkurban@gmail.com>
2025-09-19 14:33:20 +0300

    Fix for biblatex options.

--- r/_startup/1.init.R
+++ r/_startup/1.init.R
@@ -1,24 +1,4 @@
-# Setup & Init
-
-## Source File Paths -----------------------------------------------------------
-r.path <- "r"
-src.folder <- "src"
-support_scripts.folder <- "support-scripts"
-support_functions.folder <- "support-functions"
-
-r.src.path <- file.path(r.path, src.folder)
-support_scripts.path <- file.path(r.src.path, support_scripts.folder)
-support_functions.path <- file.path(r.src.path, support_functions.folder)
-
-## Setup -----------------------------------------------------------------------
-setup_script.file_path <- file.path(support_scripts.path, "setup.R")
-
-source(setup_script.file_path, 
-       catch.aborts = TRUE,
-       echo = TRUE,
-       spaced = TRUE,
-       verbose = TRUE,
-       keep.source = TRUE)
+# Init
 
 ## Define Logging Functions ----------------------------------------------------
 log_func_script.file_path <- file.path(support_functions.path, "logging-functions.R")
--- r/_startup/_run-all.R
+++ r/_startup/_run-all.R
@@ -1,11 +1,19 @@
 # Run All Scripts
 ## Source File Paths -----------------------------------------------------------
 startup.path <- "r/_startup"
+setup.script_path <- file.path(startup.path, "0.setup.R")
 init.script_path <- file.path(startup.path, "1.init.R")
 run_main.script_path <- file.path(startup.path, "2.run-main-script.R")
 rmd_render.script_path <- file.path(startup.path, "3.rmd.render.R")
 
 ## Run Scripts -----------------------------------------------------------------
+
+source(setup.script_path, 
+       catch.aborts = TRUE,
+       echo = TRUE,
+       spaced = TRUE,
+       verbose = TRUE,
+       keep.source = TRUE)
 
 source(init.script_path, 
        catch.aborts = TRUE,
